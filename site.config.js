59458fdd16ce953740ea891671f3b16ae69ea76e
LeeJungmin <94729893+Jungminii-1114@users.noreply.github.com>
2026-07-02 16:08:00 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Jungminii",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "Student studying A.I",
+    bio: "I am a student interested in Computer Vision.",
+    email: "ljm03090@gmail.com",
+    linkedin: "None",
+    github: "Jungminii-1114",
     instagram: "",
   },
   projects: [
@@ -18,8 +18,8 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Min-log",
+    description: "welcome to Min-log!",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
 
